62ac35ce16d5d3b3cfde2506e6083a200c1c0f68
JacksenSE <109568831+JacksenSE@users.noreply.github.com>
2023-02-19 21:25:49 -0800

    new version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import CarouselPage from './Components/Carousel/Carousel';
+import AboutUs from './Components/AboutUs/AboutUs';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar } from 'react-bootstrap';
+import NavbarComp from './Components/Navbar/NavbarComp';
+import styled from 'styled-components';
+import Carousel from "./Components/Carousel/Carousel"
+import PlantProfiles from './Components/PlantProfiles/PlantProfiles';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><div className="App">
+      <CarouselPage />
+      <NavbarComp />
+      <h1 style={styles}>House-Plant Watering Log</h1>
+      <PlantProfiles />
+    </div><BrowserRouter>
+        <nav>
+          <Link to="/"></Link>
+          <Link to="/create"></Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<PlantProfiles/>} />
+          <Route path="/create" element={<NavbarComp />} />
+          <Route path="/:id" element={<Carousel />} />
+        </Routes>
+      </BrowserRouter></>
+      
+     
   );
+  
 }
-
+const styles = {
+  color: 'green',
+  backgroundColor: 'salmon',
+  fontSize: '40px',
+  margin: '20px'
+}
 export default App;
